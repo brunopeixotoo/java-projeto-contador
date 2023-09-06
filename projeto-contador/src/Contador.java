adf00887c6590f7780fcf139bbc13c4d6bfbbf3d
Bruno Rodrigues Peixoto <134175465+brunopeixotoo@users.noreply.github.com>
2023-09-06 16:11:58 -0400

    Update Contador.java
    Atualização de informações sobre o programa.

--- projeto-contador/src/Contador.java
+++ projeto-contador/src/Contador.java
@@ -1,6 +1,10 @@
 /**
- * O projeto é um pequeno contador de dois valores.
- * Tem como objetivo exercitar o uso de Estruturas Excepicionais.
+ * <b>Note:</b> Leia atentamente a documentação desta classe
+ * para desfrutar dos recursos oferecidos pelo autor.
+ *
+ * O projeto é um pequeno contador de dois valores. Vai subtrair o segundo parâmetro pelo primeiro.
+ * Tem como objetivo exercitar o uso de Estruturas Excepicionais. é possível verificar o uso do TRY(cujo objetivo é disparar o erro
+ * em caso de identificado um) e o CATCH(com objetivo de alertar o usuário sobre esse erro de input).
  * Além disso, também exercita o uso de métodos e criação de exceções personalizadas.
  * 
  * @author Bruno Rodrigues Peixoto
@@ -8,6 +12,10 @@
  * @since 05/09/2023
  * 
  */
+/*
+*O uso do método Scanner é para buscar uma melhor interatividade com o usuário.
+*
+*/
 import java.util.Scanner;
 import java.util.Locale;
 
@@ -34,7 +42,8 @@ public class Contador {
  * Esse método vai fazer a contagem dos dois parâmetros enviados pelo usuário no output.
  * Além disso, é possível verificar uma exceção personalizada para caso o primeiro parâmetro
  * seja menor que o segudo parâmetro.
- * 
+ * É necessário lembrar que precisa ser criada uma exceção em outro arquivo(como fiz no segundo arquivo desse repositório).
+ * Caso contrário não será possível identificar a exceção.
  */
     }
     static void contar(int numeroUm, int numeroDois) throws ParametrosInvalidosException {
